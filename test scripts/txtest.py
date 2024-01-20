ba2a15309b6278e1c0c6754748c1fa1503211c08
WhackFather <147020181+whackfather@users.noreply.github.com>
2024-01-20 12:06:51 -0600

    Create txtest.py

---
+++ test scripts/txtest.py
@@ -0,0 +1,24 @@
+import board
+import digitalio
+import adafruit_rfm9x
+from time import sleep
+from adafruit_ms8607 import MS8607
+
+RADIO_FREQ_MHZ = 915.0
+CS = digitalio.DigitalInOut(board.RFM_CS)
+RESET = digitalio.DigitalInOut(board.RFM_RST)
+rfm95 = adafruit_rfm9x.RFM9x(board.SPI(), CS, RESET, RADIO_FREQ_MHZ)
+i2c = board.I2C()
+sensor = MS8607(i2c)
+led = digitalio.DigitalInOut(board.LED)
+led.direction = digitalio.Direction.OUTPUT
+
+while True:
+	pres = sensor.temperature
+	rfm95.send(str(pres))
+	packet = rfm95.receive(timeout=0.1)
+	if packet is not None:
+		led.value = True
+		sleep(0.1)
+	else:
+		led.value = False
