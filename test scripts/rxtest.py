5e96ddd9f6eb6c9b976b83ab9d321fe76ee9e750
WhackFather <147020181+whackfather@users.noreply.github.com>
2024-01-25 13:46:05 -0600

    Update rxtest.py

--- test scripts/rxtest.py
+++ test scripts/rxtest.py
@@ -3,7 +3,7 @@ import digitalio
 import adafruit_rfm9x
 from time import sleep
 
-RADIO_FREQ_MHZ = 915.0
+RADIO_FREQ_MHZ = 928.0
 CS = digitalio.DigitalInOut(board.RFM_CS)
 RESET = digitalio.DigitalInOut(board.RFM_RST)
 rfm95 = adafruit_rfm9x.RFM9x(board.SPI(), CS, RESET, RADIO_FREQ_MHZ)
@@ -16,8 +16,6 @@ while True:
 		led.value = True
 		packet_text = str(packet, 'ascii')
 		print(packet_text)
-		rfm95.send("a")
-		sleep(0.1)
 	else:
 		led.value = False
 		print("No signal.")
