654d6f2a37b1a6ab9ef531dca6e40748415fb1ac
WhackFather <147020181+whackfather@users.noreply.github.com>
2024-02-08 09:42:27 -0600

    Update txmain.py

--- transmission/txmain.py
+++ transmission/txmain.py
@@ -1,8 +1,9 @@
 import board
+import busio
 import digitalio
 import adafruit_rfm9x
 from adafruit_ms8607 import MS8607
-import adafruit_adxl34x
+import adafruit_bno055
 import time
 
 RADIO_FREQ_MHZ = 928.0
@@ -11,11 +12,11 @@ RESET = digitalio.DigitalInOut(board.RFM_RST)
 rfm95 = adafruit_rfm9x.RFM9x(board.SPI(), CS, RESET, RADIO_FREQ_MHZ)
 i2c = board.I2C()
 ms = MS8607(i2c)
-adxl = adafruit_adxl34x.ADXL345(i2c)
+bno = adafruit_bno055.BNO055_I2C(i2c)
 led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
 
-accel = adxl.acceleration
+accel = bno.linear_acceleration
 accel = str(accel)
 pres = ms.pressure
 temp = ms.temperature
@@ -23,7 +24,7 @@ start = time.monotonic()
 rfm95.send(str(pres) + "," + str(temp) + "," + accel.replace(" ", "").replace("(", "").replace(")", "") + ",0")
 
 while True:
-    accel = adxl.acceleration
+    accel = bno.linear_acceleration
     accel = str(accel)
     pres = ms.pressure
     temp = ms.temperature
