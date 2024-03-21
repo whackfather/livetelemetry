29ae280919f09be47b934c8dfffbe9d1c72c8f6c
WhackFather <147020181+whackfather@users.noreply.github.com>
2024-03-21 13:13:21 -0500

    Update txacceltest.py

--- test scripts/txacceltest.py
+++ test scripts/txacceltest.py
@@ -1,16 +1,31 @@
-import time
 import board
+import busio
 import digitalio
-import adafruit_adxl34x
+import adafruit_rfm9x
+import adafruit_bno055
+import time
 
+RADIO_FREQ_MHZ = 928.0
+CS = digitalio.DigitalInOut(board.RFM_CS)
+RESET = digitalio.DigitalInOut(board.RFM_RST)
+rfm95 = adafruit_rfm9x.RFM9x(board.SPI(), CS, RESET, RADIO_FREQ_MHZ)
 i2c = board.I2C()
-accelerometer = adafruit_adxl34x.ADXL345(i2c)
+bno = adafruit_bno055.BNO055_I2C(i2c)
 led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
 
+accel = bno.linear_acceleration
+accel = str(accel)
+start = time.monotonic()
+rfm95.send(accel.replace(" ", "").replace("(", "").replace(")", "") + ",0")
+
 while True:
-	led.value = True
-	print(accelerometer.acceleration)
-	time.sleep(0.05)
-	led.value = False
-	time.sleep(0.05)
+    accel = bno.linear_acceleration
+    accel = str(accel)
+    euler = bno.euler
+    end = time.monotonic()
+    elapsed = end - start
+    start = time.monotonic()
+    led.value = True
+    rfm95.send(accel.replace(" ", "").replace("(", "").replace(")", "") + "," + str(elapsed))
+    led.value = False
