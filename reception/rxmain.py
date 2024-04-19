3700ea2326a4914fe4b81dbf2d7940d6ddd76650
WhackFather <147020181+whackfather@users.noreply.github.com>
2024-04-19 09:34:40 -0500

    Update rxmain.py

--- reception/rxmain.py
+++ reception/rxmain.py
@@ -1,7 +1,7 @@
 import board
 import digitalio
 import adafruit_rfm9x
-from time import sleep
+import time
 
 RADIO_FREQ_MHZ = 928.0
 CS = digitalio.DigitalInOut(board.RFM_CS)
@@ -9,8 +9,10 @@ RESET = digitalio.DigitalInOut(board.RFM_RST)
 rfm95 = adafruit_rfm9x.RFM9x(board.SPI(), CS, RESET, RADIO_FREQ_MHZ)
 led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
+packet_text = "0,0,0,0,0"
 
 while True:
+    start = time.monotonic()
     packet = rfm95.receive()
     if packet is not None:
         led.value = True
@@ -18,4 +20,8 @@ while True:
         print(packet_text)
     else:
         led.value = False
-        print("No signal.")
+        ls_packet = list(packet_text.split(","))
+        end = time.monotonic()
+        elapsed = end - start
+        ls_packet[4] = elapsed
+        print(str(ls_packet).replace("[", "").replace("'", "").replace(" ", "").replace("]", ""))
