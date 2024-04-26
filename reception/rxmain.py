941c727d82bd06e581da136a3efb5adb19e576ca
WhackFather <147020181+whackfather@users.noreply.github.com>
2024-04-26 11:29:08 -0500

    Added packet loss timing to receiver

--- reception/rxmain.py
+++ reception/rxmain.py
@@ -10,18 +10,26 @@ rfm95 = adafruit_rfm9x.RFM9x(board.SPI(), CS, RESET, RADIO_FREQ_MHZ)
 led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
 packet_text = "0,0,0,0,0"
+lost = 0
 
 while True:
-    start = time.monotonic()
     packet = rfm95.receive()
     if packet is not None:
-        led.value = True
-        packet_text = str(packet, 'ascii')
-        print(packet_text)
+        if lost == 1:
+            lost = False
+            led.value = True
+            packet_text = str(packet, 'ascii')
+            ls_packet = list(packet_text.split(","))
+            end = time.monotonic()
+            elapsed = end - start
+            ls_packet[4] = elapsed
+            print(str(ls_packet).replace("[", "").replace("'", "").replace(" ", "").replace("]", ""))
+        elif lost == 0:
+            led.value = True
+            packet_text = str(packet, 'ascii')
+            print(packet_text)
     else:
         led.value = False
-        ls_packet = list(packet_text.split(","))
-        end = time.monotonic()
-        elapsed = end - start
-        ls_packet[4] = elapsed
-        print(str(ls_packet).replace("[", "").replace("'", "").replace(" ", "").replace("]", ""))
+        if lost == 0:
+            lost = 1
+            start = time.monotonic()
