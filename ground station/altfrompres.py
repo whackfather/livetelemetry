baa52f94476be7ff6f7349eb5480130851835a26
WhackFather <147020181+whackfather@users.noreply.github.com>
2024-05-02 09:19:33 -0500

    updated version number on altfrompres.py

--- ground station/altfrompres.py
+++ ground station/altfrompres.py
@@ -1,5 +1,5 @@
 # Altitude from Air Pressure
-# v0.1
+# v1.0
 # Roman Rodriguez
 
 import math
@@ -8,7 +8,7 @@ pres = float(input("Test pressure: "))
 
 def altitude(x):
 	if x >= 22632.11:
-		alt = (288/-0.0065) * ((x / 101325) ** ((-8.31432 * -0.0065)/(9.80665 * 0.0289644)) - 1)
+		alt = (288 / -0.0065) * ((x / 101325) ** ((-8.31432 * -0.0065) / (9.80665 * 0.0289644)) - 1)
 		return alt
 	elif x < 22632.11:
 		alt = 11000 + ((8.31432 * 216.5 * math.log(x / 22632.11)) / (-9.80665 * 0.0289644))
